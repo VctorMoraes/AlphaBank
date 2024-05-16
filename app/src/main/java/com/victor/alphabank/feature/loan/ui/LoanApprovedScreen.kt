b3a13f63fe784ae9719b3b816c13e5f36140d4e9
VctorMoraes <victorm.fernandes@outlook.com>
2024-05-16 12:38:05 +0100

    Change icon colors

--- app/src/main/java/com/victor/alphabank/feature/loan/ui/LoanApprovedScreen.kt
+++ app/src/main/java/com/victor/alphabank/feature/loan/ui/LoanApprovedScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,7 +35,7 @@ fun LoanApprovedScreen(maxAmount: Float) {
             modifier = Modifier.size(128.dp),
             imageVector = Icons.Rounded.Done,
             contentDescription = "success",
-            tint = Color.Green
+            tint = colorResource(R.color.green)
         )
 
         Text(
--- app/src/main/java/com/victor/alphabank/feature/loan/ui/LoanDeniedScreen.kt
+++ app/src/main/java/com/victor/alphabank/feature/loan/ui/LoanDeniedScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +33,7 @@ fun LoanDeniedScreen() {
             modifier = Modifier.size(128.dp),
             imageVector = Icons.Rounded.Close,
             contentDescription = "success",
-            tint = Color.Red
+            tint = colorResource(R.color.red)
         )
 
         Text(
